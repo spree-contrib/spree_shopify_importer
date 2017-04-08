dea8c9c9d0f856682e8bbf65d227dc89087a1df1
Argonus <argonuspiotr@gmail.com>
2017-04-08 14:14:58 +0200

    Add data feed model with basic validations

---
+++ spec/factories/shopify/data_feed_factory.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :shopify_data_feed, class: Shopify::DataFeed do
+    sequence(:shopify_object_id)
+    shopify_object_type 'ShopifyAPI::Product'
+    sequence(:spree_object_id)
+    spree_object_type 'Spree::Product'
+    data_feed ''
+  end
+end
