a513bb04f93b897163ed4653c239c920158484b6
Argonus <argonuspiotr@gmail.com>
2017-04-08 14:11:09 +0200

    Add migration for create shopify data feed

---
+++ db/migrate/20170407184247_create_shopify_data_feeds.rb
@@ -0,0 +1,15 @@
+class CreateShopifyDataFeeds < ActiveRecord::Migration
+  def change
+    create_table :shopify_data_feeds do |t|
+      t.integer :shopify_object_id, limit: 8, null: false
+      t.string :shopify_object_type, null: false
+      t.integer :spree_object_id
+      t.string :spree_object_type
+      t.text :data_feed
+      t.timestamps
+    end
+
+    add_index :shopify_data_feeds, [:shopify_object_id, :shopify_object_type], name: 'index_shopify_object_id_shopify_object_type', unique: true
+    add_index :shopify_data_feeds, [:spree_object_id, :spree_object_type], name: 'index_spree_object_id_spree_object_type', unique: true
+  end
+end
