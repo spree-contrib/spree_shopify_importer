ce36329d53e3b341167bc24c86e55ed63116b096
Argonus <argonuspiotr@gmail.com>
2017-07-08 17:34:00 +0200

    Add shopify line item data creator

---
+++ app/services/shopify_import/creators/line_item.rb
@@ -0,0 +1,32 @@
+module ShopifyImport
+  module Creators
+    class LineItem
+      def initialize(shopify_line_item, shopify_order, spree_order)
+        @shopify_line_item = shopify_line_item
+        @shopify_order = shopify_order
+        @spree_order = spree_order
+      end
+
+      def save
+        line_item = @spree_order.line_items.find_or_initialize_by(variant: spree_variant)
+        line_item.assign_attributes(line_item_attributes)
+        line_item.save(validate: false)
+        line_item
+      end
+
+      private
+
+      def line_item_attributes
+        parser.line_item_attributes
+      end
+
+      def spree_variant
+        parser.variant
+      end
+
+      def parser
+        @parser ||= ShopifyImport::DataParsers::LineItems::BaseData.new(@shopify_line_item, @shopify_order)
+      end
+    end
+  end
+end
