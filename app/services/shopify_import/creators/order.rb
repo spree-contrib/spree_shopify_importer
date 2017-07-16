79064fcad3604c0983e54d7f1b91827d1df81df5
Argonus <argonuspiotr@gmail.com>
2017-07-16 18:29:06 +0200

    Add creation for payments to order creator.

--- app/services/shopify_import/creators/order.rb
+++ app/services/shopify_import/creators/order.rb
@@ -6,6 +6,12 @@ module ShopifyImport
           @spree_order = create_spree_order
           assign_spree_order_to_data_feed
           create_spree_line_items
+          create_spree_payments
+          # TODO: shipments
+          # TODO: taxes
+          # TODO: promotions
+          # TODO: refunds
+          # TODO: addresses
         end
         @spree_order.update_columns(order_timestamps)
       end
@@ -23,6 +29,47 @@ module ShopifyImport
         shopify_order.line_items.each do |shopify_line_item|
           ShopifyImport::Creators::LineItem.new(shopify_line_item, shopify_order, @spree_order).save
         end
+      end
+
+      def create_spree_payments
+        transactions = shopify_order.transactions.reject { |t| t.kind.eql?('refund') }
+
+        # TODO: to verify
+        if children_transactions?(transactions)
+          ids = transactions.map(&:id)
+
+          transactions.each do |t|
+            check_transaction_is_not_duplicate?(ids, t) && create_spree_payment(t)
+          end
+        else
+          transactions.each { |t| create_spree_payment(t) }
+        end
+      end
+
+      def check_transaction_is_not_duplicate?(ids, t)
+        (!t.kind.eql?('authorization') && ids.include?(t.parent_id))
+      end
+
+      def children_transactions?(transactions)
+        check_transactions_kinds?(transactions) && check_transactions_parents?(transactions)
+      end
+
+      def check_transactions_kinds?(transactions)
+        kinds = transactions.map(&:kind)
+
+        kinds.include?('authorization') && kinds.include?('capture')
+      end
+
+      def check_transactions_parents?(transactions)
+        ids = transactions.map(&:id)
+        parent_ids = transactions.map(&:parent_id)
+
+        (parent_ids - ids).compact.empty?
+      end
+
+      def create_spree_payment(transaction)
+        transaction_data_feed = Shopify::DataFeeds::Create.new(transaction, @shopify_data_feed).save!
+        ShopifyImport::Creators::Transaction.new(transaction_data_feed, @spree_order).save!
       end
 
       def user
