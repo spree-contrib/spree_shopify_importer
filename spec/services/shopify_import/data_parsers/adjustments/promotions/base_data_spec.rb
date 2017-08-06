1fe20c415c118bb84804daeb6e9255d803d7ee51
Argonus <argonuspiotr@gmail.com>
2017-08-06 13:20:48 +0200

    Add promotion adjustment base data parser

---
+++ spec/services/shopify_import/data_parsers/adjustments/promotions/base_data_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe ShopifyImport::DataParsers::Adjustments::Promotions::BaseData, type: :service do
+  let(:spree_order) { create(:order) }
+  let(:spree_promotion) { create(:promotion) }
+  let(:shopify_discount_code) { create(:shopify_discount_code) }
+  subject { described_class.new(spree_order, spree_promotion, shopify_discount_code) }
+
+  describe '#adjustment_attributes' do
+    let(:action) { Spree::Promotion::Actions::CreateAdjustment.last }
+    let(:result) do
+      {
+        source: action,
+        order: spree_order,
+        amount: - shopify_discount_code.amount.to_d,
+        state: :closed,
+        label: shopify_discount_code.code,
+        adjustable: spree_order
+      }
+    end
+
+    it 'creates spree promotion action' do
+      expect { subject.adjustment_attributes }.to change(Spree::Promotion::Actions::CreateAdjustment, :count).by(1)
+    end
+
+    it 'returns hash of spree adjustment attributes' do
+      expect(subject.adjustment_attributes).to eq result
+    end
+  end
+
+  describe '#adjustment_timestamps' do
+    let(:result) do
+      {
+        created_at: spree_order.created_at,
+        updated_at: spree_order.updated_at
+      }
+    end
+
+    it 'returns hash of spree adjustment timestamps' do
+      expect(subject.adjustment_timestamps).to eq result
+    end
+  end
+end
