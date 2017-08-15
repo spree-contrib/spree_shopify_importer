9cdf504ef604717fa0abd1d09044a3c38b95287a
Argonus <argonuspiotr@gmail.com>
2017-08-15 12:37:52 +0200

    Refactor product fetcher to use job

--- app/services/shopify_import/data_fetchers/products_fetcher.rb
+++ app/services/shopify_import/data_fetchers/products_fetcher.rb
@@ -1,14 +1,16 @@
 module ShopifyImport
   module DataFetchers
     class ProductsFetcher < BaseFetcher
+      def import!
+        resources.each do |resource|
+          ShopifyImport::Importers::ProductImporterJob.perform_later(resource.to_json)
+        end
+      end
+
       private
 
       def resources
         ShopifyImport::Product.all(@params)
-      end
-
-      def creator
-        ShopifyImport::Creators::ProductCreator
       end
     end
   end
