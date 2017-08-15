9e1b0489ec88287e042dfdc8e989777fedc44104
Argonus <argonuspiotr@gmail.com>
2017-08-15 12:16:13 +0200

    Refactor specs

--- spec/jobs/shopify_import/invoker_job_spec.rb
+++ spec/jobs/shopify_import/invoker_job_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe ShopifyImport::InvokerJob, type: :job do
   subject { described_class }
 
-  describe '#perform_later' do
-    it 'calls invoker service', :perform_enqueued do
+  describe '#perform' do
+    it 'calls invoker service' do
       expect(ShopifyImport::Invoker).to receive(:new).and_call_original
       expect_any_instance_of(ShopifyImport::Invoker).to receive(:import!)
 
-      described_class.perform_later
+      described_class.new.perform
     end
   end
 end
