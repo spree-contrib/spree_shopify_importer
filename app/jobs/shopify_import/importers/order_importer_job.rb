a32a80a48f2ea77ee004e7736efd3d7c3c2eeef2
Argonus <argonuspiotr@gmail.com>
2017-08-20 12:45:34 +0200

    Add order importer job

---
+++ app/jobs/shopify_import/importers/order_importer_job.rb
@@ -0,0 +1,9 @@
+module ShopifyImport
+  module Importers
+    class OrderImporterJob < ApplicationJob
+      def perform(resource)
+        OrderImporter.new(resource).import!
+      end
+    end
+  end
+end
