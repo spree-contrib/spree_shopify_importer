215e43dfec7000e6b8874b18998d6222479906b9
Argonus <argonuspiotr@gmail.com>
2017-08-30 16:48:00 +0200

    Add addresses creation to order create process

--- spec/services/shopify_import/importers/order_importer_spec.rb
+++ spec/services/shopify_import/importers/order_importer_spec.rb
@@ -6,7 +6,15 @@ describe ShopifyImport::Importers::OrderImporter, type: :service do
   before { authenticate_with_shopify }
 
   describe '#import!', vcr: { cassette_name: 'shopify_import/importers/order_importer' } do
-    let(:resource) { ShopifyAPI::Order.find(5_182_437_124).to_json }
+    let!(:shopify_order) { ShopifyAPI::Order.find(5_182_437_124) }
+    let!(:user) { create(:user) }
+    let!(:user_data_feed) do
+      create(:shopify_data_feed,
+             spree_object: user,
+             shopify_object_id: shopify_order.customer.id,
+             shopify_object_type: 'ShopifyAPI::Customer')
+    end
+    let(:resource) { shopify_order.to_json }
 
     it 'creates shopify data feeds' do
       expect { subject.import! }.to change(Shopify::DataFeed, :count).by(5)
