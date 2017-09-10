fe9212a387f785f08c81c276bbd6d904006b979f
Argonus <argonuspiotr@gmail.com>
2017-09-10 13:15:51 +0200

    Add delegator to rescue api limit

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -3,4 +3,5 @@ Spree::AppConfiguration.class_eval do
   preference :shopify_password, :string
   preference :shopify_shop_domain, :string
   preference :shopify_token, :string
+  preference :shopify_rescue_limit, :integer, default: 5
 end
