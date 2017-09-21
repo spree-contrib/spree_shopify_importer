6c0e926bb97127453fc018d040ffd70bf2b855f6
Argonus <argonuspiotr@gmail.com>
2017-09-21 17:07:53 +0200

    Add update action in base importer.
    - Spec can be unit after updaters will be implemented.

--- spec/services/shopify_import/importers/base_importer_spec.rb
+++ spec/services/shopify_import/importers/base_importer_spec.rb
@@ -20,7 +20,20 @@ RSpec.describe ShopifyImport::Importers::BaseImporter, type: :service do
       let(:expected_message) { I18n.t('errors.not_implemented.creator') }
 
       it 'raises not implemented error for creator' do
-        allow_any_instance_of(described_class).to receive(:shopify_object).and_return(double('ShopifyObject'))
+        allow_any_instance_of(described_class).to receive(:shopify_object).and_return(double('ShopifyObject')
+                                                                                        .as_null_object)
+
+        expect { described_class.new.import! }.to raise_error(NotImplementedError).with_message(expected_message)
+      end
+    end
+
+    context 'updater' do
+      let(:expected_message) { I18n.t('errors.not_implemented.updater') }
+
+      xit 'raises not implemented error for updater' do
+        allow_any_instance_of(described_class).to receive(:find_existing_data_feed).and_return(double('DataFeed'))
+        allow_any_instance_of(described_class).to receive(:shopify_object).and_return(double('ShopifyObject')
+                                                                                        .as_null_object)
 
         expect { described_class.new.import! }.to raise_error(NotImplementedError).with_message(expected_message)
       end
