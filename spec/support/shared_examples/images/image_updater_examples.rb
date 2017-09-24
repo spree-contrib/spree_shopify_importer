870193e5fc610d42eecfdf3e320937d118ccdffd
Argonus <argonuspiotr@gmail.com>
2017-09-24 10:32:03 +0200

    Add image updater

---
+++ spec/support/shared_examples/images/image_updater_examples.rb
@@ -0,0 +1,33 @@
+shared_examples_for 'updates spree image' do
+  it 'creates spree image' do
+    expect { subject.update! }.to change(Spree::Image, :count).by(1)
+  end
+
+  context 'sets attributes' do
+    before { subject.update! }
+
+    it 'attachment' do
+      expect(spree_image.attachment).to be_present
+    end
+
+    it 'position' do
+      expect(spree_image.position).to eq shopify_image.position
+    end
+
+    it 'alt' do
+      expect(spree_image.alt).to eq 'Screenshot_2017-03-03_14.45.16_29b97e8b-f008-460f-8733-b33d551d7140.png'
+    end
+  end
+
+  context 'sets timestamps' do
+    before { subject.update! }
+
+    it 'created at' do
+      expect(spree_image.created_at.to_datetime).to eq shopify_image.created_at.to_datetime
+    end
+
+    it 'updated at' do
+      expect(spree_image.updated_at.to_datetime).to eq shopify_image.updated_at.to_datetime
+    end
+  end
+end
