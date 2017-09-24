cd6cc1546a562f744051d98278963d9f382fb88c
Argonus <argonuspiotr@gmail.com>
2017-09-24 16:10:09 +0200

    Refactor user importer to use create&update logic

--- app/services/shopify_import/importers/user_importer.rb
+++ app/services/shopify_import/importers/user_importer.rb
@@ -1,10 +1,28 @@
 module ShopifyImport
   module Importers
     class UserImporter < BaseImporter
+      def import!
+        data_feed = process_data_feed
+
+        if (spree_object = data_feed.spree_object).blank?
+          creator.new(data_feed).create!
+        else
+          updater.new(data_feed, spree_object).update!
+        end
+      end
+
       private
 
+      def process_data_feed
+        (old_data_feed = find_existing_data_feed).blank? ? create_data_feed : update_data_feed(old_data_feed)
+      end
+
       def creator
-        ShopifyImport::Creators::UserCreator
+        ShopifyImport::DataSavers::Users::UserCreator
+      end
+
+      def updater
+        ShopifyImport::DataSavers::Users::UserUpdater
       end
 
       def shopify_class
--- spec/services/shopify_import/importers/user_importer_spec.rb
+++ spec/services/shopify_import/importers/user_importer_spec.rb
@@ -6,7 +6,8 @@ describe ShopifyImport::Importers::UserImporter, type: :service do
   before { authenticate_with_shopify }
 
   describe '#import!', vcr: { cassette_name: 'shopify_import/importers/user_importer/import' } do
-    let(:resource) { ShopifyAPI::Customer.find(5_667_226_244).to_json }
+    let(:shopify_customer) { ShopifyAPI::Customer.find(5_667_226_244) }
+    let(:resource) { shopify_customer.to_json }
 
     it 'creates shopify data feeds' do
       expect { subject.import! }.to change(Shopify::DataFeed, :count).by(1)
@@ -14,6 +15,40 @@ describe ShopifyImport::Importers::UserImporter, type: :service do
 
     it 'creates spree users' do
       expect { subject.import! }.to change(Spree.user_class, :count).by(1)
+    end
+
+    context 'with existing data feed' do
+      let!(:shopify_data_feed) do
+        create(:shopify_data_feed,
+               shopify_object_id: shopify_customer.id,
+               shopify_object_type: 'ShopifyAPI::Customer',
+               data_feed: resource)
+      end
+
+      it 'creates shopify data feeds' do
+        expect { subject.import! }.not_to change(Shopify::DataFeed, :count)
+      end
+
+      it 'creates spree users' do
+        expect { subject.import! }.to change(Spree.user_class, :count).by(1)
+      end
+
+      context 'and user' do
+        let!(:shopify_data_feed) do
+          create(:shopify_data_feed,
+                 shopify_object_id: shopify_customer.id,
+                 shopify_object_type: 'ShopifyAPI::Customer',
+                 data_feed: resource, spree_object: create(:user))
+        end
+
+        it 'creates shopify data feeds' do
+          expect { subject.import! }.not_to change(Shopify::DataFeed, :count)
+        end
+
+        it 'creates spree users' do
+          expect { subject.import! }.not_to change(Spree.user_class, :count)
+        end
+      end
     end
   end
 end
