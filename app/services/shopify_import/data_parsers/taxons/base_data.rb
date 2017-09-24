260645f238650167752d3d022f596c3dcda97994
Argonus <argonuspiotr@gmail.com>
2017-09-24 15:08:10 +0200

    Refactor taxon data parser

--- app/services/shopify_import/data_parsers/taxons/base_data.rb
+++ app/services/shopify_import/data_parsers/taxons/base_data.rb
@@ -6,8 +6,8 @@ module ShopifyImport
           @shopify_custom_collection = shopify_custom_collection
         end
 
-        def taxon_attributes
-          {
+        def attributes
+          @attributes ||= {
             name: @shopify_custom_collection.title,
             permalink: @shopify_custom_collection.handle,
             description: @shopify_custom_collection.body_html
@@ -15,11 +15,20 @@ module ShopifyImport
         end
 
         def product_ids
-          ids = @shopify_custom_collection.products.map(&:id)
-
-          Shopify::DataFeed
-            .where(shopify_object_id: ids, shopify_object_type: 'ShopifyAPI::Product')
+          @product_ids ||=
+            Shopify::DataFeed
+            .where(shopify_object_id: collection_product_ids, shopify_object_type: 'ShopifyAPI::Product')
             .pluck(:spree_object_id)
+        end
+
+        def taxonomy
+          @taxonomy ||= Spree::Taxonomy.find_or_create_by(name: I18n.t(:shopify_custom_collections))
+        end
+
+        private
+
+        def collection_product_ids
+          @shopify_custom_collection.products.map(&:id)
         end
       end
     end
