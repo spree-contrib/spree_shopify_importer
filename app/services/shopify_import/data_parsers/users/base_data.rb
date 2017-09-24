4952248709dec7e3d95d4c9a99855aa77dae5041
Argonus <argonuspiotr@gmail.com>
2017-09-24 15:46:48 +0200

    Refactor user data parser

--- app/services/shopify_import/data_parsers/users/base_data.rb
+++ app/services/shopify_import/data_parsers/users/base_data.rb
@@ -6,13 +6,18 @@ module ShopifyImport
           @shopify_customer = shopify_customer
         end
 
-        def user_attributes
-          {
+        def attributes
+          @attributes ||= {
             created_at: @shopify_customer.created_at,
             email: @shopify_customer.email,
+            login: @shopify_customer.email,
             first_name: @shopify_customer.first_name,
             last_name: @shopify_customer.last_name
           }
+        end
+
+        def temp_password
+          @temp_password ||= SecureRandom.hex(64)
         end
       end
     end
