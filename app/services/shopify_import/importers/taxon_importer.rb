457a3d498fa16fdc729248a899de58a2d44821ee
Argonus <argonuspiotr@gmail.com>
2017-09-24 15:29:06 +0200

    Refactor taxon importer

--- app/services/shopify_import/importers/taxon_importer.rb
+++ app/services/shopify_import/importers/taxon_importer.rb
@@ -1,10 +1,28 @@
 module ShopifyImport
   module Importers
     class TaxonImporter < BaseImporter
+      def import!
+        data_feed = process_data_feed
+
+        if (spree_object = data_feed.spree_object).blank?
+          creator.new(data_feed).create!
+        else
+          updater.new(data_feed, spree_object).update!
+        end
+      end
+
       private
 
+      def process_data_feed
+        (old_data_feed = find_existing_data_feed).blank? ? create_data_feed : update_data_feed(old_data_feed)
+      end
+
       def creator
-        ShopifyImport::Creators::TaxonCreator
+        ShopifyImport::DataSavers::Taxons::TaxonCreator
+      end
+
+      def updater
+        ShopifyImport::DataSavers::Taxons::TaxonUpdater
       end
 
       def shopify_class
--- spec/services/shopify_import/importers/taxon_importer_spec.rb
+++ spec/services/shopify_import/importers/taxon_importer_spec.rb
@@ -6,7 +6,8 @@ describe ShopifyImport::Importers::TaxonImporter, type: :service do
   before { authenticate_with_shopify }
 
   describe '#import!', vcr: { cassette_name: 'shopify_import/importers/taxon_importer/import' } do
-    let(:resource) { ShopifyAPI::CustomCollection.find(:first).to_json }
+    let(:shopify_custom_collection) { ShopifyAPI::CustomCollection.find(:first) }
+    let(:resource) { shopify_custom_collection.to_json }
 
     it 'creates shopify data feeds' do
       expect { subject.import! }.to change(Shopify::DataFeed, :count).by(1)
@@ -19,6 +20,52 @@ describe ShopifyImport::Importers::TaxonImporter, type: :service do
     it 'creates spree taxons' do
       # One of taxons is taxonomy root.
       expect { subject.import! }.to change(Spree::Taxon, :count).by(2)
+    end
+
+    context 'with existing data feed' do
+      let!(:shopify_data_feed) do
+        create(:shopify_data_feed,
+               shopify_object_id: shopify_custom_collection.id,
+               shopify_object_type: 'ShopifyAPI::CustomCollection',
+               data_feed: resource)
+      end
+
+      it 'does not create shopify data feeds' do
+        expect { subject.import! }.not_to change(Shopify::DataFeed, :count)
+      end
+
+      it 'creates spree taxonomy' do
+        expect { subject.import! }.to change(Spree::Taxonomy, :count).by(1)
+      end
+
+      it 'creates spree taxons' do
+        # One of taxons is taxonomy root.
+        expect { subject.import! }.to change(Spree::Taxon, :count).by(2)
+      end
+
+      context 'and taxon' do
+        let!(:shopify_data_feed) do
+          create(:shopify_data_feed,
+                 shopify_object_id: shopify_custom_collection.id,
+                 shopify_object_type: 'ShopifyAPI::CustomCollection',
+                 data_feed: resource, spree_object: spree_taxon)
+        end
+        let(:spree_taxon) { create(:taxon, taxonomy: spree_taxonomy, parent: spree_taxonomy.root) }
+        let(:spree_taxonomy) { create(:taxonomy, name: I18n.t('shopify_custom_collections')) }
+
+        it 'does not create shopify data feeds' do
+          expect { subject.import! }.not_to change(Shopify::DataFeed, :count)
+        end
+
+        it 'does not create spree taxonomy' do
+          expect { subject.import! }.not_to change(Spree::Taxonomy, :count)
+        end
+
+        it 'does not create spree taxon' do
+          # One of taxons is taxonomy root.
+          expect { subject.import! }.not_to change(Spree::Taxon, :count)
+        end
+      end
     end
   end
 end
