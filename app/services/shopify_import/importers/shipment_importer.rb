7cb3a5fc1a9074f15028e9876472fd6f4988ff1c
Argonus <argonuspiotr@gmail.com>
2017-09-25 10:40:24 +0200

    Add multiple integration spec. Clear order data in multiple import

--- app/services/shopify_import/importers/shipment_importer.rb
+++ app/services/shopify_import/importers/shipment_importer.rb
@@ -8,14 +8,23 @@ module ShopifyImport
       end
 
       def import!
-        shopify_data_feed = create_data_feed
-        creator.new(shopify_data_feed, @parent_feed, @spree_order).create!
+        feed = ((old_data_feed = find_existing_data_feed).blank? ? create_data_feed : update_data_feed(old_data_feed))
+
+        creator.new(feed, @parent_feed, @spree_order).create!
       end
 
       private
 
+      def find_existing_data_feed
+        Shopify::DataFeed.find_by(shopify_object_id: @fulfillment.id, shopify_object_type: @fulfillment.class.to_s)
+      end
+
       def create_data_feed
         Shopify::DataFeeds::Create.new(@fulfillment, @parent_feed).save!
+      end
+
+      def update_data_feed(old_data_feed)
+        Shopify::DataFeeds::Update.new(old_data_feed, @fulfillment, @parent_feed).update!
       end
 
       def creator
--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -27,4 +27,35 @@ RSpec.feature 'end to end import' do
     expect(Spree::InventoryUnit.count).to eq 8
     expect(Spree::Address.count).to eq 5
   end
+
+  it 'multiple imports successfully', vcr: { cassette_name: 'multiple_integration' } do
+    perform_enqueued_jobs do
+      ShopifyImport::Invoker.new(
+        credentials: {
+          api_key: '0a9445b7b067719a0af024610364ee34', password: '800f97d6ea1a768048851cdd99a9101a',
+          shop_domain: 'spree-shopify-importer-test-store.myshopify.com'
+        }
+      ).import!
+      ShopifyImport::Invoker.new(
+        credentials: {
+          api_key: '0a9445b7b067719a0af024610364ee34', password: '800f97d6ea1a768048851cdd99a9101a',
+          shop_domain: 'spree-shopify-importer-test-store.myshopify.com'
+        }
+      ).import!
+    end
+
+    expect(Spree::Product.count).to eq 2
+    expect(Spree::Variant.count).to eq 4
+    expect(Spree::Image.count).to eq 3
+    expect(Spree.user_class.count).to eq 3
+    expect(Spree::Taxonomy.count).to eq 1
+    expect(Spree::Taxon.count).to eq 3
+    expect(Spree::Order.count).to eq 1
+    expect(Spree::LineItem.count).to eq 2
+    expect(Spree::Payment.count).to eq 1
+    expect(Spree::Shipment.count).to eq 3
+    expect(Spree::ShippingRate.count).to eq 3
+    expect(Spree::InventoryUnit.count).to eq 8
+    expect(Spree::Address.count).to eq 5
+  end
 end
