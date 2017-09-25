31c56f0cff9d60a3965e139fed738e3e4206ce76
Argonus <argonuspiotr@gmail.com>
2017-09-25 14:51:33 +0200

    Refactor payment updater

--- app/services/shopify_import/importers/payment_importer.rb
+++ app/services/shopify_import/importers/payment_importer.rb
@@ -10,24 +10,10 @@ module ShopifyImport
       def import!
         data_feed = process_data_feed
 
-        if (spree_object = data_feed.spree_object).blank?
-          creator.new(data_feed, @spree_order).create!
-        else
-          # Currently order import logic is deleting old data
-          spree_object.destroy
-          creator.new(data_feed, @spree_order).update!
-        end
+        creator.new(data_feed, @spree_order).create!
       end
 
       private
-
-      def process_data_feed
-        (old_data_feed = find_existing_data_feed).blank? ? create_data_feed : update_data_feed(old_data_feed)
-      end
-
-      def find_existing_data_feed
-        Shopify::DataFeed.find_by(shopify_object_id: @transaction.id, shopify_object_type: @transaction.class.to_s)
-      end
 
       def create_data_feed
         Shopify::DataFeeds::Create.new(@transaction, @parent_feed).save!
@@ -39,6 +25,10 @@ module ShopifyImport
 
       def creator
         ShopifyImport::DataSavers::Payments::PaymentCreator
+      end
+
+      def shopify_object
+        @transaction
       end
     end
   end
