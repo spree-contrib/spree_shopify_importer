6a87a9a2a92f6897bee65c3e0852d2a21905526d
Argonus <argonuspiotr@gmail.com>
2017-10-06 09:33:33 +0200

    Add required: false to data feed

--- app/models/shopify/data_feed.rb
+++ app/models/shopify/data_feed.rb
@@ -1,7 +1,7 @@
 module Shopify
   class DataFeed < ApplicationRecord
-    belongs_to :spree_object, polymorphic: true
-    belongs_to :parent, class_name: 'Shopify::DataFeed'
+    belongs_to :spree_object, polymorphic: true, required: false
+    belongs_to :parent, class_name: 'Shopify::DataFeed', required: false
     has_many :children, class_name: 'Shopify::DataFeed', foreign_key: :parent_id, dependent: :destroy
 
     validates :shopify_object_id, :shopify_object_type, presence: true
