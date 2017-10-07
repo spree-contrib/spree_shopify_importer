33e81f91d8743521c1f8dcb05423a64e22e3d7f8
Argonus <argonuspiotr@gmail.com>
2017-10-07 20:22:36 +0200

    Add refund base data parser

---
+++ app/services/shopify_import/data_parsers/refunds/base_data.rb
@@ -0,0 +1,41 @@
+module ShopifyImport
+  module DataParsers
+    module Refunds
+      class BaseData
+        def initialize(shopify_refund, shopify_transaction, spree_reimbursement = nil)
+          @shopify_refund = shopify_refund
+          @shopify_transaction = shopify_transaction
+          @spree_reimbursement = spree_reimbursement
+        end
+
+        def attributes
+          @attributes ||= {
+            payment: payment,
+            amount: @shopify_transaction.amount,
+            transaction_id: @shopify_transaction.authorization,
+            reason: reason,
+            reimbursement: @spree_reimbursement
+          }
+        end
+
+        def timestamps
+          @timestamps ||= {
+            created_at: @shopify_refund.created_at,
+            updated_at: @shopify_refund.processed_at
+          }
+        end
+
+        private
+
+        def payment
+          Shopify::DataFeed.find_by(shopify_object_id: @shopify_transaction.parent_id,
+                                    shopify_object_type: 'ShopifyAPI::Transaction').try(:spree_object)
+        end
+
+        def reason
+          Spree::RefundReason.create_with(active: false).find_or_create_by(name: I18n.t(:shopify))
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/shopify_import/data_parsers/refunds/base_data_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe ShopifyImport::DataParsers::Refunds::BaseData, type: :service do
+  let(:shopify_refund) { create(:shopify_refund) }
+  let(:shopify_transaction) { create(:shopify_transaction, parent_id: 12_345) }
+  let(:spree_reimbursement) { create(:reimbursement) }
+
+  subject { described_class.new(shopify_refund, shopify_transaction, spree_reimbursement) }
+
+  describe '#attributes' do
+    let(:payment) { create(:payment) }
+    let(:reason) { Spree::RefundReason.find_by!(name: I18n.t(:shopify)) }
+    let!(:shopify_data_feed) do
+      create(:shopify_data_feed,
+             shopify_object_id: shopify_transaction.parent_id,
+             shopify_object_type: 'ShopifyAPI::Transaction',
+             spree_object: payment)
+    end
+    let(:result) do
+      {
+        payment: payment,
+        amount: shopify_transaction.amount,
+        transaction_id: shopify_transaction.authorization,
+        reason: reason,
+        reimbursement: spree_reimbursement
+      }
+    end
+
+    it 'returns hash of refund attributes' do
+      expect(subject.attributes).to eq result
+    end
+  end
+
+  describe '#timestamps' do
+    let(:result) do
+      {
+        created_at: shopify_refund.created_at,
+        updated_at: shopify_refund.processed_at
+      }
+    end
+
+    it 'returns hash of refund timestamps' do
+      expect(subject.timestamps).to eq result
+    end
+  end
+end
