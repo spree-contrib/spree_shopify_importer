48219c6b2f0ea0de458504818a411539b3d076a4
Argonus <argonuspiotr@gmail.com>
2017-10-07 20:27:18 +0200

    Add reimbursement base data parser

---
+++ spec/services/shopify_import/data_parsers/reimbursements/base_data_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe ShopifyImport::DataParsers::Reimbursements::BaseData, type: :service do
+  let(:shopify_refund) { create(:shopify_refund) }
+  let(:spree_customer_return) { create(:customer_return) }
+  let(:spree_order) { create(:order) }
+
+  subject { described_class.new(shopify_refund, spree_customer_return, spree_order) }
+
+  describe '#number' do
+    it 'returns reimbursement number' do
+      expect(subject.number).to eq "SRI#{shopify_refund.id}"
+    end
+  end
+
+  describe '#attributes' do
+    let(:result) do
+      {
+        reimbursement_status: :reimbursed,
+        customer_return: spree_customer_return,
+        order: spree_order,
+        total: 0
+      }
+    end
+
+    it 'returns hash of reimbursement attributes' do
+      expect(subject.attributes).to eq result
+    end
+  end
+
+  describe '#timestamps' do
+    let(:result) do
+      {
+        created_at: shopify_refund.created_at,
+        updated_at: shopify_refund.processed_at
+      }
+    end
+
+    it 'returns hash of reimbursement timestamps' do
+      expect(subject.timestamps).to eq result
+    end
+  end
+end
