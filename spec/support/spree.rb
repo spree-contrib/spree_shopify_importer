504db82af7963719282706636db27b8c38797d03
Argonus <argonuspiotr@gmail.com>
2017-12-14 12:39:12 +0100

    Refactor factory girl to factory bot in specs support

--- spec/support/spree.rb
+++ spec/support/spree.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
 
   config.before :each do
     reset_spree_preferences do |spree_config|
-      spree_config.default_country_id = FactoryGirl.create(:country).id
+      spree_config.default_country_id = FactoryBot.create(:country).id
     end
   end
 end
