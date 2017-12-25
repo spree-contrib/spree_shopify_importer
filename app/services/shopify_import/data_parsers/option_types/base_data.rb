f197fef50a453940c11ffe5d1fcb6547fecc997a
Argonus <argonuspiotr@gmail.com>
2017-12-25 18:29:45 +0100

    Add option types base data parser

---
+++ app/services/shopify_import/data_parsers/option_types/base_data.rb
@@ -0,0 +1,26 @@
+module ShopifyImport
+  module DataParsers
+    module OptionTypes
+      class BaseData
+        def initialize(option_type)
+          @option_type = option_type
+        end
+
+        def attributes
+          {
+            name: name,
+            presentation: @option_type.name
+          }
+        end
+
+        def name
+          @name ||= @option_type.name.downcase
+        end
+
+        def shopify_values
+          @option_type.values.compact
+        end
+      end
+    end
+  end
+end
