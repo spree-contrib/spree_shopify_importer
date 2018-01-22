03ea815b7d9b9a2efc85adcae21c87324ccd85f6
Argonus <argonuspiotr@gmail.com>
2018-01-22 17:27:47 +0100

    Add aggregate failures to items creation - feature test

--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -13,24 +13,26 @@ RSpec.feature 'end to end import' do
       ).import!
     end
 
-    expect(Spree::Product.count).to eq 2
-    expect(Spree::Variant.count).to eq 4
-    expect(Spree::Image.count).to eq 3
-    expect(Spree.user_class.count).to eq 3
-    expect(Spree::Taxonomy.count).to eq 1
-    expect(Spree::Taxon.count).to eq 3
-    expect(Spree::Order.count).to eq 1
-    expect(Spree::LineItem.count).to eq 2
-    expect(Spree::Payment.count).to eq 1
-    expect(Spree::Shipment.count).to eq 3
-    expect(Spree::ShippingRate.count).to eq 3
-    expect(Spree::InventoryUnit.count).to eq 8
-    expect(Spree::Address.count).to eq 5
-    expect(Spree::ReturnAuthorization.count).to eq 1
-    expect(Spree::ReturnItem.count).to eq 1
-    expect(Spree::CustomerReturn.count).to eq 1
-    expect(Spree::Reimbursement.count).to eq 1
-    expect(Spree::Refund.count).to eq 1
+    aggregate_failures 'items creation' do
+      expect(Spree::Product.count).to eq 2
+      expect(Spree::Variant.count).to eq 4
+      expect(Spree::Image.count).to eq 3
+      expect(Spree.user_class.count).to eq 3
+      expect(Spree::Taxonomy.count).to eq 1
+      expect(Spree::Taxon.count).to eq 3
+      expect(Spree::Order.count).to eq 1
+      expect(Spree::LineItem.count).to eq 2
+      expect(Spree::Payment.count).to eq 1
+      expect(Spree::Shipment.count).to eq 3
+      expect(Spree::ShippingRate.count).to eq 3
+      expect(Spree::InventoryUnit.count).to eq 8
+      expect(Spree::Address.count).to eq 5
+      expect(Spree::ReturnAuthorization.count).to eq 1
+      expect(Spree::ReturnItem.count).to eq 1
+      expect(Spree::CustomerReturn.count).to eq 1
+      expect(Spree::Reimbursement.count).to eq 1
+      expect(Spree::Refund.count).to eq 1
+    end
   end
 
   it 'multiple imports successfully', vcr: { cassette_name: 'multiple_integration' } do
@@ -49,23 +51,25 @@ RSpec.feature 'end to end import' do
       ).import!
     end
 
-    expect(Spree::Product.count).to eq 2
-    expect(Spree::Variant.count).to eq 4
-    expect(Spree::Image.count).to eq 3
-    expect(Spree.user_class.count).to eq 3
-    expect(Spree::Taxonomy.count).to eq 1
-    expect(Spree::Taxon.count).to eq 3
-    expect(Spree::Order.count).to eq 1
-    expect(Spree::LineItem.count).to eq 2
-    expect(Spree::Payment.count).to eq 1
-    expect(Spree::Shipment.count).to eq 3
-    expect(Spree::ShippingRate.count).to eq 3
-    expect(Spree::InventoryUnit.count).to eq 8
-    expect(Spree::Address.count).to eq 5
-    expect(Spree::ReturnAuthorization.count).to eq 1
-    expect(Spree::ReturnItem.count).to eq 1
-    expect(Spree::CustomerReturn.count).to eq 1
-    expect(Spree::Reimbursement.count).to eq 1
-    expect(Spree::Refund.count).to eq 1
+    aggregate_failures 'items creation' do
+      expect(Spree::Product.count).to eq 2
+      expect(Spree::Variant.count).to eq 4
+      expect(Spree::Image.count).to eq 3
+      expect(Spree.user_class.count).to eq 3
+      expect(Spree::Taxonomy.count).to eq 1
+      expect(Spree::Taxon.count).to eq 3
+      expect(Spree::Order.count).to eq 1
+      expect(Spree::LineItem.count).to eq 2
+      expect(Spree::Payment.count).to eq 1
+      expect(Spree::Shipment.count).to eq 3
+      expect(Spree::ShippingRate.count).to eq 3
+      expect(Spree::InventoryUnit.count).to eq 8
+      expect(Spree::Address.count).to eq 5
+      expect(Spree::ReturnAuthorization.count).to eq 1
+      expect(Spree::ReturnItem.count).to eq 1
+      expect(Spree::CustomerReturn.count).to eq 1
+      expect(Spree::Reimbursement.count).to eq 1
+      expect(Spree::Refund.count).to eq 1
+    end
   end
 end
